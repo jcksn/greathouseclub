2584eb01ae62d4a69108dbf5d04c601311a5660e
Isaac Rosenberg <irosenb7@gmail.com>
2015-05-20 21:33:10 -0700

    Change message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
     @twilio = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
     @twilio.messages.create(
       from: "+16504378953",
-      to: @user.phone,
-      body: "Welcome to the club. I'll need your Instagram and Snapchat. I'm gonna make you look like a gentleman. - Greathouse Falls"
+      to: self.phone,
+      body: "Yo! Welcome to the Greathouse Club.  Here's how the Club works: To get in, you text this number with your instagram/other social media account info. After a quick check, I add you on snapchat. Once you're in, you can snap a picture for any kind of fashion advice -- deciding between two things, tips on how to wear something, whatever you want. Totally free."
     )   
   end
 end
